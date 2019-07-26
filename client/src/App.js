fb023bc9aedd9ad11ce7fd77a26cc398f092116c
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-07-26 01:04:44 -0700

    Redux setup, userReducer, uiReducer

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import jwtDecode from 'jwt-decode';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 // Pages
 import home from './pages/home';
 import login from './pages/login';
@@ -33,28 +37,30 @@ if (token) {
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home} />
-              <AuthRoute
-                exact
-                path="/login"
-                component={login}
-                authenticated={authenticated}
-              />
-              <AuthRoute
-                exact
-                path="/signup"
-                component={signup}
-                authenticated={authenticated}
-              />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <AuthRoute
+                  exact
+                  path="/login"
+                  component={login}
+                  authenticated={authenticated}
+                />
+                <AuthRoute
+                  exact
+                  path="/signup"
+                  component={signup}
+                  authenticated={authenticated}
+                />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </Provider>
     </MuiThemeProvider>
   );
 }
---
+++ client/src/redux/store.js
@@ -0,0 +1,25 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import userReducer from './reducers/userReducer';
+import dataReducer from './reducers/dataReducer';
+import uiReducer from './reducers/uiReducer';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const reducers = combineReducers({
+  user: userReducer,
+  data: dataReducer,
+  UI: uiReducer
+});
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
